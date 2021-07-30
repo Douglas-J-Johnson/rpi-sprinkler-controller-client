75acdc86350d71eb10837564c05a4f7a116b4c90
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-07-30 15:30:51 -0600

    add initial code outline for github oauth api service

---
+++ src/api/github.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import qs from 'qs';
+
+const BASE_URL = 'https://github.com/login/oauth/'
+const CLIENT_ID = '4a8495585bb1c3214624';
+// const CLIENT_SECRET = null; // only required for token request
+const STATE = 'secret-sauce';
+
+const OAUTH_CODE_QUERY_PARAMS = {
+    client_id: CLIENT_ID,
+    state: STATE
+}
+const OAUTH_CODE_URL = `${BASE_URL}/authorize?${qs.stringify(OAUTH_CODE_QUERY_PARAMS)}`;
+// let OAUTH_TOKEN_QUERY_PARAMS = {
+//     client_id: CLIENT_ID,
+//     client_secret: CLIENT_SECRET,
+//     code = null // retreived from initial oauth temporary code request
+// }
+
+export default {
+    getOauthCode() {
+        let code = null;
+        // perform GET to OAUTH_CODE_URL
+        return code;
+    },
+    // async getOauthToken () {
+    //     let token = null;
+    //     let OAUTH_TOKEN_URL = null
+    //     // perform GET to OAUTH_CODE_URL to get temporary code and set OAUTH_TOKEN_QUERY_PARAMS.code = code;
+        
+    //     OAUTH_TOKEN_URL = `${BASE_URL}/access_token?${qs.stringify(OAUTH_TOKEN_QUERY_PARAMS)}`;
+    //     // perform POST to OAUTH_TOKEN_URL
+
+    //     return token;
+    // }
+}
