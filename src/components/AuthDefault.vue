7a61d9bc38911c68e2ef15384542a2a254e58f28
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-04 12:05:09 -0600

    complete skeleton of AuthDefault component

--- src/components/AuthDefault.vue
+++ src/components/AuthDefault.vue
@@ -1,38 +1,26 @@
 <template>
     <div id="auth-defualt">
-        ERROR
+        Authorization Failed.
     </div>
 </template>
 
 <script>
-// import qs from 'qs';
-// import config from '../config';
+import qs from 'qs';
+import { router } from '../main'
 
 export default {
     name: 'AuthHandler',
     created() {
-        console.log('ERROR');
+        // currently, when a user rejects permission application is redirected to 'Authorization callback URL' specified in the Github oauth app
+        const QUERY_PARAMS = qs.parse(window.location.search.slice(1));
+
+        if (Object.prototype.hasOwnProperty.call(QUERY_PARAMS, 'error')) {
+            console.log('OAuth Error', QUERY_PARAMS.error);
+        }
+        else { console.log('OAuth Error', 'Unspecified OAuth error.'); }
+
+        router.push('/');
     }
-    // methods: {
-    //     search() {
-    //         // currently, when a user rejects permission application is redirected to 'Authorization callback URL' specified in the Github oauth app
-
-    //         const QUERY_PARAMS = qs.parse(window.location.search.slice(1));
-
-    //         if (Object.prototype.hasOwnProperty.call(QUERY_PARAMS, 'error') && Object.prototype.hasOwnProperty.call(QUERY_PARAMS, 'error_description')) {
-    //             console.log('OAuth Error', QUERY_PARAMS.error_description);
-    //         }
-    //         else if (Object.prototype.hasOwnProperty.call(QUERY_PARAMS, 'code')) {
-    //             if (Object.prototype.hasOwnProperty.call(QUERY_PARAMS, 'state') && QUERY_PARAMS.state === config.github.accessCodeState) {
-    //                 this.finalizeLogin(QUERY_PARAMS.code);
-    //                 return "OAuth Login Successful";
-    //             }
-    //         }
-    //         else { console.log('OAuth Error', 'Unspecified OAuth error.'); }
-
-    //         return "OAuth Login Failed.";
-    //     }
-    // }
 };
 </script>
 
