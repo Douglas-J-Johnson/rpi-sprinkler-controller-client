b25312982c55ccbef2846c3c6d139cb4200681c8
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-03 19:16:00 -0600

    clean up semicolons and comment out unused TOKEN_REDIRECT_URL line

--- src/api/github.js
+++ src/api/github.js
@@ -1,9 +1,9 @@
 import qs from 'qs';
 
-const BASE_URL = 'https://github.com/login/oauth'
+const BASE_URL = 'https://github.com/login/oauth';
 const CLIENT_ID = '4a8495585bb1c3214624';
-const CODE_REDIRECT_URI = 'http://localhost:8080/oauth/callback/access-code'
-const TOKEN_REDIRECT_URI = 'http://localhost:8080/oauth/callback/token'
+const CODE_REDIRECT_URI = 'http://localhost:8080/oauth/callback/access-code';
+// const TOKEN_REDIRECT_URI = 'http://localhost:8080/oauth/callback/token';
 const STATE = 'secret-sauce';
 // const CLIENT_SECRET = null; // only required for token request
 
@@ -11,7 +11,7 @@ const OAUTH_CODE_QUERY_PARAMS = {
     client_id: CLIENT_ID,
     redirect_uri: CODE_REDIRECT_URI,
     state: STATE
-}
+};
 const OAUTH_CODE_URL = `${BASE_URL}/authorize?${qs.stringify(OAUTH_CODE_QUERY_PARAMS)}`;
 
 export default {
@@ -31,4 +31,4 @@ export default {
     //     OAUTH_TOKEN_URL = `${BASE_URL}/access_token?${qs.stringify(OAUTH_TOKEN_QUERY_PARAMS)}`;
     //     // perform POST to OAUTH_TOKEN_URL
     // }
-}
+};
