0ead225b83591aa3a98643489e33cd5be848920b
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-12 13:50:35 -0600

    add devices vuex module

---
+++ src/store/modules/devices.js
@@ -0,0 +1,40 @@
+const state = {
+    devices: [
+        {id: 1, name: 'Pump', type: 'switch'},
+        {id: 2, name: 'Zone 1', type: 'switch'},
+        {id: 3, name: 'Zone 2', type: 'switch'},
+    ] 
+};
+
+const getters = {
+    allDevices: (state) => { return state.devices; },
+    deviceByID: (state, id) => {
+        // if found {
+        //  return device object
+        //} else
+            return null;
+        //}
+    }
+};
+
+const mutations = {
+    addDevice: (state, device) => {
+        // check to see if device is in devices
+        // if already in list, warn
+        // else add device to list
+    },
+    updateDevice: (state, id) => {
+        // check to see if device is in devices
+        // if already in list, update
+        // else error
+    },
+    removeDevice: (state, id) => {
+        // check to see if device is in devices
+        // if already in list, delete
+        // else info message
+    }
+};
+
+const actions = { };
+
+export default { state, getters, mutations, actions };
