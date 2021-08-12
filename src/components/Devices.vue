fc1901bba1273c1f8de43e906caf2ecca0460daf
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-12 11:23:25 -0600

    Add Devices and Schedules components

---
+++ src/components/Devices.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="devices" class="page">
+        <h3>Devices</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Devices'
+}
+</script>
+
+<style scoped>
+    #devices { }
+</style>
---
+++ src/components/Schedules.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="schedules" class="page">
+        <h3>Schedules</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Schedules'
+}
+</script>
+
+<style scoped>
+    #schedules { }
+</style>
