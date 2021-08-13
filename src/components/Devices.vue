dbe65adae3b7075e9ef6e817ffb09816bf847ab2
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-12 19:38:41 -0600

    initial device list rendering

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -1,12 +1,26 @@
 <template>
     <div id="devices" class="page">
-        <h3>Devices</h3>
+    <h3>Devices</h3>
+        <div class="ui divided list">
+            <div v-for="device in allDevices" :key="device.id" class="item">
+                <div class="content">
+                    <a class="header">{{ device.name }}</a>
+                    <div class="description">{{ device.type }}</div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-    name: 'Devices'
+    name: 'Devices',
+    computed: {
+        ...mapGetters(['allDevices'])
+    }
 }
 </script>
 
