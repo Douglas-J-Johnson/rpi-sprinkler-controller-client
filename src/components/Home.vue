b169b9465fab3dc2ad2eab9bba286e489c68ac42
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-09-09 07:19:35 -0600

    -m

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,18 +5,21 @@
         </div>
         <div v-else>
             <DevicesSummary></DevicesSummary>
+            <SchedulesSummary></SchedulesSummary>
         </div>
     </div>
 </template>
 
 <script>
 import DevicesSummary from './DevicesSummary.vue'
+import SchedulesSummary from './SchedulesSummary.vue'
 import { mapGetters } from 'vuex';
 
 export default {
     name: "Home",
     components: {
-        DevicesSummary
+        DevicesSummary,
+        SchedulesSummary
     },
     computed: {
         ...mapGetters(['isLoggedIn']),
---
+++ src/components/SchedulesSummary.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="schedules-summary">
+        <h3>Schedules</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SchedulesSummary'
+}
+</script>
+
+<style scoped>
+    #schedules-summary { 
+        margin-top: 1rem;
+    }
+</style>
