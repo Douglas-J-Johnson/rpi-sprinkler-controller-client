58b7fbaf54b76d90f969628bdd32ac771a9e3d63
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-09-09 07:09:52 -0600

    add a device summary to Home page

---
+++ src/components/DevicesSummary.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="devices-summary">
+    <h3>Devices</h3>
+        <div class="ui divided list">
+            <div v-for="device in allDevices" :key="device.channel" class="item">
+                <div class="left floated content">
+                    <button v-if="device.assigned"
+                        class="circular ui button"
+                        :class="{ green: deviceOn(device), grey: !deviceOn(device) }"></button>
+                    <button v-else
+                        class="circular disabled basic ui button"></button>
+                </div>
+                <div class="content">
+                    <a class="header">{{ device.name }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'DevicesSummary',
+    computed: {
+        ...mapGetters(['allDevices'])
+    },
+    methods: {
+        deviceOn: function (device) { return device.state === 1 ? true : false; }
+    }
+}
+</script>
+
+<style scoped>
+    #devices-summary { }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,20 @@
             <h3>Please Log in</h3>
         </div>
         <div v-else>
-            <h3>You are logged in!</h3>
+            <DevicesSummary></DevicesSummary>
         </div>
     </div>
 </template>
 
 <script>
+import DevicesSummary from './DevicesSummary.vue'
 import { mapGetters } from 'vuex';
 
 export default {
     name: "Home",
+    components: {
+        DevicesSummary
+    },
     computed: {
         ...mapGetters(['isLoggedIn']),
     }
